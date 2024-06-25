978ffeceead81dcac9a5ab81fc3aa6336a186c21
ur434548 <usman.gcu218@gmail.com>
2024-06-25 18:26:22 +0500

    Making filter Option

--- src/App.js
+++ src/App.js
@@ -7,10 +7,36 @@ import ExpenseForm from "./NewExpense/ExpenseForm";
 import ExpenseFilter from "./Expense/ExpenseFilter";
 
 const cars = [
-  { id: 'e1', name: 'Toilet Paper', amount: 94.12, date: new Date(2020, 7, 14) },
-  { id: 'e2',name: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-  { id: 'e3', name: 'Car Insurance', amount: 294.67, date: new Date(2022, 2, 28) },
-  { id: 'e4', name: 'New Desk ', amount: 450, date: new Date(2023, 5, 12) },
+  {
+    id: "1",
+    date: new Date(2020, 11, 20),
+    name: "Suzuki",
+    amount: 1599.6,
+  },
+  {
+    id: "2",
+    date: new Date(2020, 11, 20),
+    name: "Toyota",
+    amount: 3299.6,
+  },
+  {
+    id: "3",
+    date: new Date(2020, 11, 20),
+    name: "Chaghan",
+    amount: 4599.0,
+  },
+  {
+    id: "4",
+    date: new Date(2020, 11, 20),
+    name: "Honda",
+    amount: 2599.22,
+  },
+  {
+    id: "5",
+    date: new Date(2020, 11, 20),
+    name: "Hundai",
+    amount: 599.6,
+  },
 ];
 
 function App() {
@@ -24,15 +50,17 @@ function App() {
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-    
   };
+  const filteredExpenses = expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <div className="App">
       <h1>Expense Tracker</h1>
       <ExpenseForm onAddExpense={addExpenseHandler} />
-      <ExpenseFilter  onChange={filterChangeHandler} />
-      {expenses.map((expense) => (
+      <ExpenseFilter selected={filteredYear} onChange={filterChangeHandler} />
+      {filteredExpenses .map((expense) => (
         <Expense
           key={expense.id}
           date={expense.date}
--- src/Expense/ExpenseFilter.js
+++ src/Expense/ExpenseFilter.js
@@ -10,11 +10,12 @@ export default function ExpenseFilter(props) {
     <div className="expense-filter">
       <div className="expense-filter__control">
         <label>Filter by Year</label>
-        <select  onChange={dropdownChanger}>
+        <select value={props.selected} onChange={dropdownChanger}>
           <option value="2020">2020</option>
           <option value="2021">2021</option>
           <option value="2022">2022</option>
           <option value="2023">2023</option>
+          <option value="2024">2024</option>
         </select>
       </div>
     </div>
